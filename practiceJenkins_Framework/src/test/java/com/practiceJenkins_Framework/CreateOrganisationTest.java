43f293ef25f9c6b58c8646e058a3d7e908a39f55
MonikaSingh9219 <monika.singh9219@gmail.com>
2022-08-16 18:05:38 +0530

    changes done

--- practiceJenkins_Framework/src/test/java/com/practiceJenkins_Framework/CreateOrganisationTest.java
+++ practiceJenkins_Framework/src/test/java/com/practiceJenkins_Framework/CreateOrganisationTest.java
@@ -32,6 +32,8 @@ public class CreateOrganisationTest {
 	System.out.println("Vtiger Org");
 	System.out.println("Test Script");
 	System.out.println("Failed");
+	System.out.println("Failed test case");
+	
 	
 }
 	
